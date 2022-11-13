223545efa7cbbdb3a7545fd464027588f5f04e8b
Martin Studna <martin.studna2@gmail.com>
2022-11-13 18:00:24 +0100

    Adding import for fonts.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
+
+const inter = Inter()
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
